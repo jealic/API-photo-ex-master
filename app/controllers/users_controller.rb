eceeaf7828700aff96825dabc26bd5fd0aca7c6f
Ling Chiehyun <jealic@gmail.com>
2018-09-06 09:44:27 +0800

    print token on layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,4 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-
-  before_create :generate_authentication_token
-
-  def generate_authentication_token
-    self.authentication_token = Devise.friendly_token    
-  end
 end
